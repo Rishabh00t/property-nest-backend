690eb72c7e77bfab9363e56cc318e42a75bad8ec
Rishabh00t <rt0580155@gmail.com>
2026-06-28 16:06:04 +0530

    remove uvicorn dependency

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.router import api_router
 from app.core.config import settings
-
+import uvicorn
 
 def create_app() -> FastAPI:
     app = FastAPI(title=settings.app_name)
@@ -22,3 +22,10 @@ def create_app() -> FastAPI:
 
 app = create_app()
 
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8001,
+        reload=True,
+    )
